ec64008adc5585c71c22819bf6cf10a71fe6dd25
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-06-22 11:38:12 -0500

    agregando ENV

--- models/server.js
+++ models/server.js
@@ -11,7 +11,7 @@ class Server {
     constructor() {
 
         this.app = express();
-        this.port = 8080;
+        this.port = process.env.PORT;
 
         // Http server
         this.server = http.createServer(this.app);
